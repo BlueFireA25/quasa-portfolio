75cfbf5a5f57f2d04343aa6865f754301ff7745e
Sebastián Castro <sebastian.castroa25@gmail.com>
2023-03-12 15:43:42 -0500

    Changes Experience

--- src/components/ExperienceProfile.vue
+++ src/components/ExperienceProfile.vue
@@ -20,143 +20,277 @@
         color="primary"
       />
     </div>
-    <q-timeline :layout="layout" color="secondary">
-      <q-timeline-entry heading>
-        Timeline heading
-        <br />
-        ({{
-          $q.screen.lt.sm ? 'Dense' : $q.screen.lt.md ? 'Comfortable' : 'Loose'
-        }}
-        layout)
-      </q-timeline-entry>
+    <q-tabs
+      v-model="tab"
+      dense
+      class="text-grey"
+      active-color="primary"
+      indicator-color="primary"
+      align="justify"
+      narrow-indicator
+    >
+      <q-tab
+        class="text-capitalize"
+        name="work"
+        label="Work"
+        style="border-radius: 16px"
+      />
+      <q-tab
+        class="text-capitalize"
+        name="education"
+        label="Education"
+        style="border-radius: 16px"
+      />
+    </q-tabs>
 
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        side="left"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
+    <q-tab-panels
+      v-model="tab"
+      animated
+      class="text-dark"
+      style="background: transparent"
+    >
+      <q-tab-panel name="work">
+        <q-timeline
+          :layout="layout"
+          style="background: transparent"
+          color="secondary"
+        >
+          <q-timeline-entry
+            title="Frontend Developer"
+            subtitle="May 2022 - Today"
+            side="right"
+            icon="bi-hospital-fill"
+            color="cyan"
+          >
+            <div class="q-mb-md text-subtitle1 text-weight-medium">
+              <div class="row items-center">
+                <div
+                  class="q-mr-sm bg-white q-pa-xs text-center"
+                  style="border-radius: 50%"
+                >
+                  <q-img
+                    src="~assets/img/logo-snabb.png"
+                    :ratio="16 / 9"
+                    width="30px"
+                    height="30px"
+                    fit="contain"
+                    spinner-color="primary"
+                    spinner-size="82px"
+                    alt="logo-snabb"
+                  />
+                </div>
+                <a href="https://snabb.cl/" target="_blank">Snabb</a>
+              </div>
+            </div>
+            <ul>
+              <li>Creation of new projects with Vue and Quasar.</li>
+              <li>REST API consumption.</li>
+              <li>Creation of Stores in Pinia.</li>
+              <li>Creating components in Vue.</li>
+              <li>FHIR documentation.</li>
+            </ul>
+          </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 21, 1986"
-        side="right"
-        icon="delete"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
+          <q-timeline-entry
+            title="Full Stack Developer"
+            subtitle="February 2021 - February 2022"
+            side="right"
+            icon="bi-building-fill"
+            color="blue"
+          >
+            <div class="q-mb-md text-subtitle1 text-weight-medium">
+              <div class="row items-center">
+                <div
+                  class="q-mr-sm bg-white q-pa-xs text-center"
+                  style="border-radius: 50%"
+                >
+                  <q-img
+                    src="~assets/img/logo-binapps.png"
+                    :ratio="16 / 9"
+                    width="30px"
+                    height="30px"
+                    fit="contain"
+                    spinner-color="primary"
+                    spinner-size="82px"
+                    alt="logo-snabb"
+                  />
+                </div>
+                <a href="https://binapps.com/" target="_blank">Binapps</a>
+              </div>
+            </div>
+            <ul>
+              <li>Participation and creation of new web applications.</li>
+              <li>
+                Correction of errors and support in the different web
+                applications of the company.
+              </li>
+              <li>Development of user interfaces.</li>
+              <li>
+                Implementation of new functions and features in its main ERP
+                software and in the different developments of the company.
+              </li>
+              <li>Bug fixes and support in your core ERP software.</li>
+            </ul>
+          </q-timeline-entry>
 
-      <q-timeline-entry heading>November, 2017</q-timeline-entry>
+          <q-timeline-entry
+            title="Software Developer (Internship)"
+            subtitle="August 2020 - February 2021"
+            side="right"
+            color="blue"
+          >
+            <div class="q-mb-md text-subtitle1 text-weight-medium">
+              <div class="row items-center">
+                <div
+                  class="q-mr-sm bg-white q-pa-xs text-center"
+                  style="border-radius: 50%"
+                >
+                  <q-img
+                    src="~assets/img/logo-binapps.png"
+                    :ratio="16 / 9"
+                    width="30px"
+                    height="30px"
+                    fit="contain"
+                    spinner-color="primary"
+                    spinner-size="82px"
+                    alt="logo-snabb"
+                  />
+                </div>
+                <a href="https://binapps.com/" target="_blank">Binapps</a>
+              </div>
+            </div>
+            <ul>
+              <li>
+                Implementation of new functions and features in its main ERP
+                software and in the different developments of the company.
+              </li>
+              <li>Bug fixes and support in your core ERP software.</li>
+            </ul>
+          </q-timeline-entry>
+        </q-timeline>
+      </q-tab-panel>
 
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        side="left"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
+      <q-tab-panel name="education">
+        <q-timeline
+          :layout="layout"
+          style="background: transparent"
+          color="secondary"
+        >
+          <q-timeline-entry
+            title="Systems Engineer"
+            subtitle="February 2021 - February 2022"
+            side="right"
+            icon="bi-mortarboard-fill"
+            color="warning"
+          >
+            <div class="q-mb-md text-subtitle1 text-weight-medium">
+              <div class="row items-center">
+                <div
+                  class="q-mr-sm bg-white q-pa-xs text-center"
+                  style="border-radius: 50%"
+                >
+                  <q-img
+                    src="~assets/img/logo-uam.jpg"
+                    :ratio="16 / 9"
+                    width="30px"
+                    height="30px"
+                    fit="contain"
+                    spinner-color="primary"
+                    spinner-size="82px"
+                    alt="logo-snabb"
+                  />
+                </div>
+                <a href="https://www.autonoma.edu.co/" target="_blank">
+                  Universidad Autónoma de Manizales
+                </a>
+              </div>
+            </div>
+          </q-timeline-entry>
 
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        side="right"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        side="left"
-        color="orange"
-        icon="done_all"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        side="right"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-
-      <q-timeline-entry
-        title="Event Title"
-        subtitle="February 22, 1986"
-        side="left"
-      >
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-      </q-timeline-entry>
-    </q-timeline>
+          <q-timeline-entry
+            title="Other Studies"
+            subtitle="All the time"
+            side="right"
+            icon="bi-hospital-fill"
+            color="cyan"
+          >
+            <ul class="text-weight-medium">
+              <li>
+                <a
+                  href="https://platzi.com/p/sebastian.castroa25/curso/2008-html-css/diploma/detalle/"
+                  target="_blank"
+                >
+                  Curso Definitivo de HTML y CSS
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://platzi.com/p/sebastian.castroa25/curso/2467-frontend-developer/diploma/detalle/"
+                  target="_blank"
+                >
+                  Curso de Frontend Developer
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://platzi.com/p/sebastian.castroa25/curso/2477-course/diploma/detalle/"
+                  target="_blank"
+                >
+                  Curso Práctico de Frontend Developer
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.udemy.com/certificate/UC-c9741149-7336-4c09-94ae-2e436c77e1ca/"
+                  target="_blank"
+                >
+                  GIT + GitHub: Todo un sistema de control de versiones de cero,
+                  Udemy
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.udemy.com/certificate/UC-876a8867-d9e6-41d2-83c6-39ac05bc4427/"
+                  target="_blank"
+                >
+                  Laravel 9 - Crea Aplicaciones y Sitios Web con PHP 8 y MVC,
+                  Udemy
+                </a>
+              </li>
+              <li class="q-pb-md">
+                <a
+                  href="https://www.freecodecamp.org/espanol/certification/BlueFire/responsive-web-design"
+                  target="_blank"
+                >
+                  Responsive Web Design, Freecodecamp
+                </a>
+              </li>
+            </ul>
+            <q-btn
+              href="https://www.linkedin.com/in/sebastianibaguecastro/"
+              target="_blank"
+              class="q-ml-lg"
+              padding="14px 10px"
+              style="border-radius: 16px"
+              no-caps
+              color="primary"
+              icon="bi-linkedin"
+              label="You can see all my courses on my LinkedIn."
+            />
+          </q-timeline-entry>
+        </q-timeline>
+      </q-tab-panel>
+    </q-tab-panels>
   </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue';
 import { useQuasar } from 'quasar';
-import { computed } from 'vue';
 
 const $q = useQuasar();
-
 const layout = computed(() => {
   return $q.screen.lt.sm ? 'dense' : $q.screen.lt.md ? 'comfortable' : 'loose';
 });
+const tab = ref('work');
 </script>
